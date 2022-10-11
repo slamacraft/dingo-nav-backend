4f4b831530b569def77df01deba7667edadcca0a
slamacraft <1114951452@qq.com>
2022-10-11 10:24:39 +0800

    abstract RepeatPlugin

--- src/main/scala/com/dingdo/plugin/repeat/RepeatPlugin.scala
+++ src/main/scala/com/dingdo/plugin/repeat/RepeatPlugin.scala
@@ -12,19 +12,20 @@ class RepeatPlugin{
 
   def repeat(msg: MessageEvent): Unit = {
     msg match {
-      case groupEvent: GroupMessageEvent => {
-        val msg = groupEvent.getMessage.contentToString()
-        val groupMsg = MsgCacheContext.group(groupEvent.getGroup.getId)
-        val toRepeat = groupMsg.msg.slice(1, repeatCount)
-          .forall(it => it.msg.endsWith(msg))
-
-        // todo 进行复读
-        if(toRepeat){
-          groupEvent.getGroup.sendMessage(msg)
-        }
-      }
+      case groupEvent: GroupMessageEvent => repeatGroupMsg _
       case _ =>
     }
+  }
 
+  def repeatGroupMsg(groupEvent: GroupMessageEvent): Unit ={
+    val msg = groupEvent.getMessage.contentToString
+    val groupMsg = MsgCacheContext.group(groupEvent.getGroup.getId)
+    val toRepeat = groupMsg.msg.slice(1, repeatCount)
+      .forall(it => it.msg.endsWith(msg))
+
+    // todo 进行复读
+    if (toRepeat) {
+      groupEvent.getGroup.sendMessage(msg)
+    }
   }
 }
