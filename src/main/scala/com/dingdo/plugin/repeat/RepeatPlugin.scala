6655065bb15e9b055bd127867ebdf6d0a3f45d65
slamacraft <1114951452@qq.com>
2022-10-11 10:32:44 +0800

    repeat update

--- src/main/scala/com/dingdo/plugin/repeat/RepeatPlugin.scala
+++ src/main/scala/com/dingdo/plugin/repeat/RepeatPlugin.scala
@@ -4,28 +4,34 @@ import com.dingdo.mirai.context.MsgCacheContext
 import com.dingdo.mirai.core.BotPlugin
 import net.mamoe.mirai.event.events.{GroupMessageEvent, MessageEvent}
 
-// todo 复读要去掉已复读过的句子
-class RepeatPlugin{
+import scala.collection.mutable
 
-  // todo 重复次数
+// 复读要去掉已复读过的句子
+class RepeatPlugin {
+
+  // 重复次数
   val repeatCount = 3
+  // 这个群上次复读得语句，如果这次复读得和上次得相同，则不复读
+  val groupRepeat = new mutable.HashMap[Long, String]()
 
   def repeat(msg: MessageEvent): Unit = {
     msg match {
-      case groupEvent: GroupMessageEvent => repeatGroupMsg _
+      case groupEvent: GroupMessageEvent => repeatGroupMsg(groupEvent)
       case _ =>
     }
   }
 
-  def repeatGroupMsg(groupEvent: GroupMessageEvent): Unit ={
+  def repeatGroupMsg(groupEvent: GroupMessageEvent): Unit = {
     val msg = groupEvent.getMessage.contentToString
-    val groupMsg = MsgCacheContext.group(groupEvent.getGroup.getId)
-    val toRepeat = groupMsg.msg.slice(1, repeatCount)
-      .forall(it => it.msg.endsWith(msg))
+    val groupId = groupEvent.getGroup.getId
 
-    // todo 进行复读
+    val groupMsg = MsgCacheContext.group(groupId)
+    val toRepeat = groupMsg.msg.slice(1, repeatCount)
+      .forall(it => it.msg.endsWith(msg) && groupRepeat(groupId) != msg)
+
     if (toRepeat) {
       groupEvent.getGroup.sendMessage(msg)
+      groupRepeat(groupId) = msg
     }
   }
 }
