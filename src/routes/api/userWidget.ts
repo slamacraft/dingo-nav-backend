df23d7e8362f947d47552950ede0bedffcc4324d
slamacraft <1114951452@qq.com>
2023-07-25 10:29:14 +0800

    添加查询套件详情接口

--- src/routes/api/userWidget.ts
+++ src/routes/api/userWidget.ts
@@ -3,8 +3,23 @@ import {Response, Router} from "express";
 import Request from "../../types/api/Request";
 import auth from "src/middleware/auth";
 import UserWidget from "src/models/UserWidget";
+import {check, validationResult} from "express-validator";
+import HttpStatusCodes from "http-status-codes";
 
 const router: Router = Router();
+
+router.get("/", auth,
+    [check("id", "Id不能为空").notEmpty()],
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(HttpStatusCodes.BAD_REQUEST)
+                .json({errors: errors.array()});
+        }
+        const userWidget = await UserWidget.findById(req.body.id)
+        res.json(userWidget);
+    })
 
 /**
  * 获取用户自定义套件列表
@@ -15,6 +30,7 @@ router.get("/list", auth, async (req: Request, res: Response) => {
     })
     res.json(userWidget);
 });
+
 
 
 export default router;
