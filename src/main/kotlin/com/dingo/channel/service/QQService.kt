d010fb133537b7e715729d895a9bf681962b4893
slamacraft <1114951452@qq.com>
2025-03-08 23:27:27 +0800

    1、删除mirai以及minio依赖

--- src/main/kotlin/com/dingo/channel/service/QQService.kt
+++ src/main/kotlin/com/dingo/channel/service/QQService.kt
@@ -7,12 +7,18 @@ import com.dingo.channel.model.VerifyVo
 import com.dingo.config.properties.QQProperty
 import com.dingo.core.dify.DifyMsgSender
 import com.dingo.core.qq.QQMsgSender
+import com.dingo.module.entity.conversation.ConversationEntity
+import com.dingo.module.entity.conversation.ConversationTable
 import net.i2p.crypto.eddsa.EdDSAEngine
 import net.i2p.crypto.eddsa.EdDSAPrivateKey
 import net.i2p.crypto.eddsa.spec.EdDSANamedCurveTable
 import net.i2p.crypto.eddsa.spec.EdDSAPrivateKeySpec
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.selectAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 open class QQService {
@@ -82,15 +88,35 @@ open class QQService {
     /**
      * 回复频道里的消息
      */
-    fun sendChannelMsg(
+    @Transactional
+    open fun sendChannelMsg(
         dto: ChannelDto, content: String = dto.content,
         inputs: MutableMap<String, Any>.() -> Unit = {}
     ): String {
-        return difyMsgSender.sendChannelMsg(
-            content, dto.author.username,
-            dto.channel_id, dto.guild_id,
-            inputs
-        )
+        // 通过channelId和guideId查询会话，如果存在则使用会话id发送消息
+        val conversation = ConversationTable.selectAll()
+            .where {
+                (ConversationTable.guideId eq dto.guild_id) and
+                        (ConversationTable.channelId eq dto.channel_id)
+            }.firstOrNull()?.let {
+                ConversationTable.buildEntity(it)
+            }
+
+        val userName = dto.author.username
+        if (conversation != null) {
+            // 如果有存在的会话，则直接使用会话id发送消息
+            val (answer, _) = difyMsgSender.sendMsg(content, userName, conversation.conversationId, inputs)
+            return answer
+        }
+
+        // 否则创建一个会话，并保存新的会话id
+        val (answer, conversationId) = difyMsgSender.sendMsg(content, userName, "", inputs)
+        ConversationTable.insert(ConversationEntity {
+            this.channelId = channelId
+            this.guideId = guideId
+            this.conversationId = conversationId
+        })
+        return answer
     }
 
 }
--- src/main/kotlin/com/dingo/core/dify/DifyMsgSender.kt
+++ src/main/kotlin/com/dingo/core/dify/DifyMsgSender.kt
@@ -3,16 +3,10 @@ package com.dingo.core.dify
 import com.dingo.config.post
 import com.dingo.config.properties.DifyProperty
 import com.dingo.config.sendRequestThenGetResp
-import com.dingo.module.entity.conversation.ConversationEntity
-import com.dingo.module.entity.conversation.ConversationTable
 import com.fasterxml.jackson.annotation.JsonProperty
 import okhttp3.Request
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.selectAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 
 @Component
@@ -20,41 +14,16 @@ open class DifyMsgSender {
     @Autowired
     lateinit var difyProperty: DifyProperty
 
-    @Transactional
-    open fun sendChannelMsg(
-        msg: String, userName: String, channelId: String, guideId: String,
-        inputs: MutableMap<String, Any>.() -> Unit = {}
-    ): String {
-        // 通过channelId和guideId查询会话，如果存在则使用会话id发送消息
-        val conversation = ConversationTable.selectAll()
-            .where {
-                (ConversationTable.guideId eq guideId) and
-                        (ConversationTable.channelId eq channelId)
-            }.firstOrNull()?.let {
-                ConversationTable.buildEntity(it)
-            }
-
-        if (conversation != null) {
-            // 如果有存在的会话，则直接使用会话id发送消息
-            val (answer, _) = sendMsg(msg, userName, conversation.conversationId, inputs)
-            return answer
-        }
-
-        // 否则创建一个会话，并保存新的会话id
-        val (answer, conversationId) = sendMsg(msg, userName, "", inputs)
-        ConversationTable.insert(ConversationEntity {
-            this.channelId = channelId
-            this.guideId = guideId
-            this.conversationId = conversationId
-        })
-        return answer
-    }
-
     /**
      * 向dify的ai发送消息
+     *
+     * @param content 消息内容
+     * @param userName 用户名
+     * @param conversationId 会话id
+     * @param inputs 输入参数，详情请参见Dify的文档
      */
-    private fun sendMsg(
-        msg: String, userName: String, conversationId: String,
+    fun sendMsg(
+        content: String, userName: String, conversationId: String,
         inputs: MutableMap<String, Any>.() -> Unit = {}
     ): Answer {
         val host = difyProperty.url
@@ -64,7 +33,7 @@ open class DifyMsgSender {
         val inputs = mutableMapOf<String, Any>().apply(inputs)
 
         val body = mapOf(
-            "query" to msg,
+            "query" to content,
             "user" to userName,
             "conversation_id" to conversationId,
             "response_mode" to "blocking",
